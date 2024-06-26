2cf9ab8624388e674c788fe5a031861b1b624dd8
whzhni <167661046+whzhni@users.noreply.github.com>
2024-06-26 15:21:25 +0800

    diy-part1.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part1.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part1.sh
@@ -15,6 +15,5 @@
 
 # Add a feed source
 # luci-theme-infinityfreedom主题
-src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
 git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 git clone https://github.com/kenzok8/small package/small
