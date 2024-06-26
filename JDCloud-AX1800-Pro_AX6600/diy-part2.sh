2808a86ffa4799f766a787994d71da40ccd3b474
whzhni <167661046+whzhni@users.noreply.github.com>
2024-06-26 12:12:43 +0800

    diy-part2.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.68.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.68.1/g' package/base-files/files/bin/config_generate
 # 去掉默认密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 ##-----------------Add dev core for kenzo OpenClash------------------
